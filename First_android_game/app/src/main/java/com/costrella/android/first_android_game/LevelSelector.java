225a27a8796dd65367743a3cff446690ecc99841
Michal Kostrzewa <misiek.mk@gmail.com>
2015-08-08 14:27:40 +0200

    2 aktywnosci:
    Main i LevelSelector
    dzialajace commity tylko !
    play i wybierz poziom dziala

---
+++ First_android_game/app/src/main/java/com/costrella/android/first_android_game/LevelSelector.java
@@ -0,0 +1,16 @@
+package com.costrella.android.first_android_game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mike on 2015-08-08.
+ */
+public class LevelSelector extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.level);
+
+    }
+}
--- First_android_game/app/src/main/java/com/costrella/android/first_android_game/MainActivity.java
+++ First_android_game/app/src/main/java/com/costrella/android/first_android_game/MainActivity.java
@@ -1,41 +1,41 @@
 package com.costrella.android.first_android_game;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
-public class MainActivity extends ActionBarActivity {
-    //siema 2
+public class MainActivity extends Activity {
+    //    ImageButton playBtn, highScoreBtn, settingsBtn;
+    Button playBtn, levelBtn;
 
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        playBtn = (Button) findViewById(R.id.btnPlay);
+        levelBtn = (Button) findViewById(R.id.btnLevel);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), LevelSelector.class);
+                startActivity(i);
+            }
+        });
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        levelBtn.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            Intent i = new Intent(getApplicationContext(), LevelSelector.class);
+                                            startActivity(i);
+                                        }
+                                    }
+        );
     }
 }
