5233607b7c27bdffc12657fa82a392c50a137068
Michal Kostrzewa <misiek.mk@gmail.com>
2015-08-08 12:54:42 +0200

    siema 2

--- First_android_game/app/src/main/java/com/costrella/android/first_android_game/MainActivity.java
+++ First_android_game/app/src/main/java/com/costrella/android/first_android_game/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-    //siema
+    //siema 2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
